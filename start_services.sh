a097c0283a1f534cacd4bb07c8a3cebb91b3f3f4
daffatm <daffatama45@gmail.com>
2021-05-09 15:47:00 +0700

    edit start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,7 @@
 cd app/
 
 # start rasa server
-rasa run --model /app/models --enable-api \
+rasa run --model /app/models --enable-api --cors "*"\
         --endpoints /app/endpoints.yml \
         --credentials /app/credentials.yml \
         -p $PORT
